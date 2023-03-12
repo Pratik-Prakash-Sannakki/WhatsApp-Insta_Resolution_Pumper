609fb939602ef07de6e433e062ef16bc7c732c30
Pratik-Prakash-Sannakki <sannakki.p@northeastern.edu>
2023-03-12 11:31:21 -0400

    flask application configured

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 import cv2
 import time
 
@@ -28,3 +29,58 @@ cv2.imshow('EDSR',edsr_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+'''
+
+import json
+from flask import Flask, request, render_template, send_file, send_from_directory
+import os
+
+app = Flask(__name__)
+
+# Specify the directory where uploaded images will be stored
+UPLOAD_FOLDER = 'uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Render the index.html template
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Handle image uploads
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Check if the POST request contains a file
+    if 'file' not in request.files:
+        return 'No file uploaded', 400
+    
+    file = request.files['file']
+    
+    # Check if the file is empty
+    if file.filename == '':
+        return 'No file selected', 400
+    
+    # Save the file to the server
+    filename = file.filename
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    return 'File uploaded successfully', 200
+
+# Serve uploaded images
+'''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    print(filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+'''
+@app.route('/get_images')
+def get_images():
+    
+    filenames = os.listdir(app.config['UPLOAD_FOLDER'])
+    print(filenames)
+    return json.dumps(filenames)
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+if __name__ == '__main__':
+    app.run(debug=True)
