0cc4b9c09f924f13808488279b8e4896dd524ccf
Pratik-Prakash-Sannakki <sannakki.p@northeastern.edu>
2023-03-12 22:55:55 -0400

    Application intial build: complete , Deploy on to Cloud Service

--- app.py
+++ app.py
@@ -1,35 +1,3 @@
-'''
-import cv2
-import time
-
-img = cv2.imread('vc.jpg')
-width = img.shape[1]
-height = img.shape[0]
-bicubic = cv2.resize(img,(width*4,height*4))
-cv2.imshow('Image',img)
-cv2.imshow('BICUBIC',bicubic)
-
-super_res = cv2.dnn_superres.DnnSuperResImpl_create()
-
-start = time.time()
-super_res.readModel('LapSRN_x4.pb')
-super_res.setModel('lapsrn',4)
-lapsrn_image = super_res.upsample(img)
-end = time.time()
-print('Time taken in seconds by lapsrn', end-start)
-cv2.imshow('LAPSRN',lapsrn_image)
-
-start = time.time()
-super_res.readModel('EDSR_x4.pb')
-super_res.setModel('edsr',4)
-edsr_image = super_res.upsample(img)
-end = time.time()
-print('Time taken in seconds by edsr', end-start)
-cv2.imshow('EDSR',edsr_image)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-'''
 import os.path as osp
 import glob
 import cv2
@@ -44,7 +12,7 @@ model_path = 'models/RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/
 device = torch.device('cpu')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
-test_img_folder = 'LR/*'
+test_img_folder = 'uploads/*'
 
 model = arch.RRDBNet(3, 3, 64, 23, gc=32)
 model.load_state_dict(torch.load(model_path), strict=True)
@@ -58,11 +26,19 @@ app = Flask(__name__)
 
 # Specify the directory where uploaded images will be stored
 UPLOAD_FOLDER = 'uploads/'
+RESULT_FOLDER = 'results/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
 # Render the index.html template
 @app.route('/')
 def index():
+    dir = 'uploads/'
+    res='results/'
+    for f in os.listdir(dir):
+        os.remove(os.path.join(dir, f))
+    for f in os.listdir(res):
+        os.remove(os.path.join(res, f))
+    
     return render_template('index.html')
 
 # Handle image uploads
@@ -81,6 +57,8 @@ def upload():
     # Save the file to the server
     filename = file.filename
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+    # ESRGAN implement on all files in 
     idx = 0
     for path in glob.glob(test_img_folder):
         idx += 1
@@ -98,25 +76,35 @@ def upload():
         output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
         output = (output * 255.0).round()
         cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+        print("image upscaled")
     
-    return 'File uploaded and resolution pumped up successfully', 200
+    return 'File uploaded and resolution pumped up successfully. Please go back to the landing page and click on Show image.', 200
 
 # Serve uploaded images
-'''
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    print(filename)
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-'''
+@app.route('/get_Uploaded_images')
+def get_Uploaded_images():
+    filenames = os.listdir(app.config['UPLOAD_FOLDER'])
+    print(filenames,"********************************intial********************************")
+    return json.dumps(filenames)
+
+# Server resultant Upscaled image
 @app.route('/get_images')
 def get_images():
-    
-    filenames = os.listdir(app.config['UPLOAD_FOLDER'])
-    print(filenames)
+    filenames = os.listdir(app.config['RESULT_FOLDER'])
+    print(filenames,"********************************results********************************")
     return json.dumps(filenames)
+
+
+# list of all uploaded image
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+# list of all uploaded image 
+@app.route('/results/<filename>')
+def result_file(filename):
+    return send_file(os.path.join(app.config['RESULT_FOLDER'], filename))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
